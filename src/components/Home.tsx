20f0f818643b71a0348c4351270e8eb751468707
Omar Khan <khancodegt@gmail.com>
2021-11-03 13:53:46 -0400

    add wallet address and xrp balance to Home view

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,25 @@
 import * as React from 'react';
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import './Home.css';
 import { GameList } from './GameList';
 
-export interface AppProps {
-   compiler: string;
-   framework: string;
-}
+export interface HomeProps {}
 
-export const Home = (props: AppProps) => {
+// TODO: style this entire component lol
+export const Home = (props: HomeProps) => {
+   const { state } = useLocation<Location>();
+   const [classicAddress, setClassicAddress] = useState(state.wallet.classicAddress);
+   const [xrpBalance, setXrpBalance] = useState(state.wallet.xrpBalance);
+
    return (
       <div className="home">
          <h1>Online React Chess!</h1>
          <h2 className="description">
-            Online chess implemented in React.js and Node.js
+            Wallet address: {classicAddress}
+         </h2>
+         <h2 className="description">
+            XRP Balance: {xrpBalance}
          </h2>
          <GameList />
       </div>
